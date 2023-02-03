a88808433db8d279ce75c1b72aee74994164a90a
bgautijonsson <bgautijonsson@gmail.com>
2023-02-03 14:39:02 +0000

    Adding new icelandic labeling functions

--- R/scales.R
+++ R/scales.R
@@ -14,6 +14,7 @@ isk <- function(x, scale = 1, suffix = NULL, ...) {
     if (is.null(suffix)) {
         scaling <- c(
             " kr" = 1,
+            " þús.kr" = 1e-3,
             " m.kr" = 1e-6,
             " ma.kr" = 1e-9
         )
@@ -48,6 +49,7 @@ label_isk <- function(scale = 1, suffix = NULL, accuracy = 1, ...) {
     if (is.null(suffix)) {
         scaling <- c(
             " kr" = 1,
+            " þús.kr" = 1e-3,
             " m.kr" = 1e-6,
             " ma.kr" = 1e-9
         )
@@ -73,8 +75,8 @@ label_isk <- function(scale = 1, suffix = NULL, accuracy = 1, ...) {
 #' Format Numbers as Percentages with Icelandic Standards
 #'
 #' @param x Numberic vector to be formatted as percentage
-#' @param ...
 #' @param accuracy A number to round to. F.ex. 0.01 shows 2 decimal places.
+#' @param ... Other inputs passed on to scales::percent()
 #'
 #' @return A character vector version of the input number, formatted as percentage with Icelandic localization
 #' @export
@@ -92,8 +94,8 @@ hlutf <- function(x, accuracy = 0.1, ...) {
 #' Create a Labeller to Format Numbers as Percentages with Icelandic Standards
 #'
 #' @param x Numberic vector to be formatted as percentage
-#' @param ...
 #' @param accuracy A number to round to. F.ex. 0.01 shows 2 decimal places.
+#' @param ... Other inputs passed on to scales::label_percent()
 #'
 #' @return A character vector version of the input number, formatted as percentage with Icelandic localization
 #' @export
@@ -101,6 +103,44 @@ hlutf <- function(x, accuracy = 0.1, ...) {
 #' @examples
 label_hlutf <- function(x, accuracy = 0.1, ...) {
     scales::label_percent(
+        big.mark = ".",
+        decimal.mark = ",",
+        accuracy = accuracy,
+        ...)
+}
+
+
+#' Format Numbers with Icelandic Standards
+#'
+#' @param x Numberic vector to be formatted as percentage
+#' @param accuracy A number to round to. F.ex. 0.01 shows 2 decimal places.
+#' @param ... Other inputs passed on to scales::number()
+#'
+#' @return A character vector version of the input number, formatted as percentage with Icelandic localization
+#' @export
+#'
+#' @examples
+tala <- function(x, accuracy = 0.1, ...) {
+    scales::number(
+        x,
+        big.mark = ".",
+        decimal.mark = ",",
+        accuracy = accuracy,
+        ...)
+}
+
+#' Create a Labeller to Format Numbers with Icelandic Standards
+#'
+#' @param x Numeric vector to be formatted as percentage
+#' @param accuracy A number to round to. F.ex. 0.01 shows 2 decimal places.
+#' #' @param ... Other inputs passed on to scales::label_number()
+#'
+#' @return A character vector version of the input number, formatted as percentage with Icelandic localization
+#' @export
+#'
+#' @examples
+label_tala <- function(x, accuracy = 0.1, ...) {
+    scales::label_number(
         big.mark = ".",
         decimal.mark = ",",
         accuracy = accuracy,
